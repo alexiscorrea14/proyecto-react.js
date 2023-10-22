57ef84f59471a2986fdc2aeb0c5a2f3dc8cfe763
alexiscorrea14 <aec181294@gmail.com>
2023-10-22 00:05:32 -0300

    comentario

---
+++ src/components/Item.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Item = ( {producto} ) => {
+  return (
+    <div className="producto">
+      <img src={producto.imagen} />
+      <div>
+        <h4>{producto.titulo}</h4>
+        <p>Precio: ${producto.precio}</p>
+        <p>Categoria: {producto.categoria}</p>
+        <p> {producto.descripcion}</p>
+        <a className="ver-mas" href={'/item/${productos.id}'}> Ver más</a>
+      </div>
+    </div>
+  )
+}
+
+export default Item
